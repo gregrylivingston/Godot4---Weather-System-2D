699155e32883e0d80514de8762ca5d6edbd8952d
gregrylivingston <livingsgb@gmail.com>
2024-08-21 11:44:09 -0400

    rain delta adjustments, rain signal refactor

--- sky/sky_setting.gd
+++ sky/sky_setting.gd
@@ -2,19 +2,21 @@
 class_name SkySetting extends Node2D
 
 
+@export_category("Rain")
+
 signal updateRainAmount
 @export_range(0.0,1.0) var rainAmount = 0.0:
 	set(newRainAmount):
 		rainAmount = newRainAmount
 		updateRainAmount.emit(newRainAmount)
+		
+@export_range(-0.1,0.1) var rainDelta = 0.0
+		
+var SkyGradient2D: GradientTexture2D = load("res://sky/Gradient2D_Sky.tres")
 
+func _ready() -> void:pass
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	await get_tree().process_frame
-	rainAmount = rainAmount
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not Engine.is_editor_hint():
+		SkyGradient2D.fill_to.y += 0.0001
+		rainAmount += rainDelta / 100.0
