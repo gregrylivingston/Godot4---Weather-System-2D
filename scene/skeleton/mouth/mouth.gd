d8276453dad98a43182a303e173ec63706a698a6
gregrylivingston <livingsgb@gmail.com>
2024-08-24 20:36:54 -0400

    more rose garden improvements

--- scene/skeleton/mouth/mouth.gd
+++ scene/skeleton/mouth/mouth.gd
@@ -12,7 +12,7 @@ var frameBuffer := 5
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if 	%AudioStreamPlayer.playing && frameBuffer < 1:
-		frameBuffer = 3
+		frameBuffer = 5
 		var volume := clampf( db_to_linear(AudioServer.get_bus_peak_volume_left_db(0,0)) , 0.1 ,1.0)
 
 		if volume < 0.11:
